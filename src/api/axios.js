63f9b017d8a0906c10b3663730e4f17b9c9d912a
Sathvik Bennihalli <sathvikbennihalli@Sathviks-MacBook-Air.local>
2024-05-25 13:16:44 +0530

    auth2

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 const apiURL = process.env.REACT_APP_API_URL;
-axios.defaults.withCredentials = true;
+
 
 export default axios.create({
+  withCredentials: true,
   baseURL: `${apiURL}`,
 });
