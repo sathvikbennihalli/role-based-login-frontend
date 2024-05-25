753061d0c904484bd6740f1853daa6c42a0d11bd
Sathvik Bennihalli <sathvikbennihalli@Sathviks-MacBook-Air.local>
2024-05-25 14:55:15 +0530

    auth7

--- src/components/axiosInstance.js
+++ src/components/axiosInstance.js
@@ -1,8 +1,24 @@
+import { config } from "@fortawesome/fontawesome-svg-core";
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL, // Your backend URL
   withCredentials: true,
 });
+
+instance.interceptors.request.use(
+    config => {
+        const authToken = Cookies.get("token");
+
+        config.headers['Cookie'] = `token=${authToken}`;
+
+        return config;
+    }, error => {
+        return Promise.reject(error);
+    }
+);
+
+
 
 export default instance;
