baea8a06131227170ffc832e73b628e129ad67f1
Sathvik Bennihalli <sathvikbennihalli@Sathviks-MacBook-Air.local>
2024-05-25 15:02:59 +0530

    auth9

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import Cookies from "js-cookie";
+
 const apiURL = process.env.REACT_APP_API_URL;
 
 axios.defaults.withCredentials = true;
--- src/components/axiosInstance.js
+++ src/components/axiosInstance.js
@@ -1,22 +1,8 @@
 import axios from "axios";
-import Cookies from "js-cookie";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL, // Your backend URL
   withCredentials: true,
 });
-
-instance.interceptors.request.use(
-  (config) => {
-    const authToken = Cookies.get("token");
-
-    config.headers["Cookie"] = `token=${authToken}`;
-
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default instance;
